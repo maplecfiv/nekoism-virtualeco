7ef75e0f3a108c0d9516b50a26d7fbf5c89084a0
twinpa <hold_on_to_love_eco@hotmail.co.jp>
2015-03-09 23:17:13 +0900

    fix client_protocol_ver

--- lib/packet/login_data_handler.py
+++ lib/packet/login_data_handler.py
@@ -60,7 +60,7 @@ class LoginDataHandler:
 		data = data_io.read()
 		self.send("ffff")
 		general.log("[login] eco version", unpack_unsigned_int(data[:4]))
-		self.send("0002", pack_int(1018)+pack_int(0)) #認証接続確認(s0001)の応答
+		self.send("0002", data) #認証接続確認(s0001)の応答
 		self.send("001e", self.word_front+self.word_back) #PASS鍵
 		general.log("[login] send word",
 			self.word_front.encode("hex"), self.word_back.encode("hex"),
